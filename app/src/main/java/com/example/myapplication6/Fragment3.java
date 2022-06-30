4283b4d10c9830fddc9f685757a88c14df7faf78
2018007983 <2018007983@hanyang.ac.kr>
2022-06-30 20:12:28 +0900

    new

--- app/src/main/java/com/example/myapplication6/Fragment3.java
+++ app/src/main/java/com/example/myapplication6/Fragment3.java
@@ -2,14 +2,20 @@ package com.example.myapplication6;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class Fragment3 extends Fragment {
+public class Fragment3 extends Fragment implements View.OnTouchListener{
+
+    TextView showXY;
+    int posX, posY;
+
     public static Fragment3 newInstance(int number) {
         Fragment3 fragment3 = new Fragment3();
         Bundle bundle = new Bundle();
@@ -30,6 +36,28 @@ public class Fragment3 extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return LayoutInflater.from(inflater.getContext()).inflate(R.layout.fragment3, container, false);
+        View view = inflater.inflate(R.layout.fragment3, container, false);
+        showXY = (TextView) view.findViewById(R.id.showXY);
+        showXY.setOnTouchListener(this);
+        return view;
     }
+
+    public boolean onTouch(View v, MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            //손가락이 화면에 닿았을 때
+            posX = (int) event.getX();
+            posY = (int) event.getY();
+            showXY.setText("X: " + posX + ",  Y: " + posY);
+        } else if (event.getAction() == MotionEvent.ACTION_MOVE) {
+            //드래그 상태
+            posX = (int) event.getX();
+            posY = (int) event.getY();
+            showXY.setText("X: " + posX + ",  Y: " + posY);
+        } else if (event.getAction() == MotionEvent.ACTION_UP) {
+            //손떼면?
+        }
+
+        return true;
+    }
+
 }
